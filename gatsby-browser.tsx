37b1b1740a8a1bdd9b6694e6939f810612236fb7
sorrel012 <wain1719@gmail.com>
2024-04-25 09:41:37 +0900

    [24.04.25] 프로젝트 root 파일 설정

---
+++ gatsby-browser.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './src/assets/css/theme';
+import { Provider } from 'react-redux';
+import store from './src/store/store';
+import './src/assets/css/common.css';
+
+const queryClient = new QueryClient();
+
+export const wrapRootElement: React.FC<{ element: React.ReactNode }> = ({
+  element,
+}) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>{element}</Provider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+};
