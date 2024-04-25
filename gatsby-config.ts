476c1cc9f674a91b19c9823afca7f1022306fd49
sorrel012 <wain1719@gmail.com>
2024-04-25 10:23:06 +0900

    [24.04.25] Contentful 설정

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,21 +1,28 @@
-import type { GatsbyConfig } from "gatsby";
+require('dotenv').config();
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `hwh-in-wonderland`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "",
-      "spaceId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components"]
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'nj30k01aow4s',
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-styled-components',
+  ],
 };
 
 export default config;
